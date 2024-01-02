1fecac9bbebe66648c5fe3c0c438aad26c25f968
송준섭 <wnstjq7979@gmail.com>
2024-01-02 19:13:45 +0900

    feat: 커맨드를 아무것도 입력하지 않았을 경우 Geet 설명 출력
    - Geet 설명을 출력해주는 guideGeet 함수 추가

--- src/main/kotlin/geet/ProcessGeet.kt
+++ src/main/kotlin/geet/ProcessGeet.kt
@@ -3,7 +3,20 @@ package geet
 fun processGeet(parseData: Map<String, String>): Unit {
     when (parseData["command"]) {
         "init" -> println("init")
-        null -> println("geet 설명")
+        null -> guideGeet()
         else -> println("geet ${parseData["command"]} is not a valid command. Try init instead.")
     }
+}
+
+fun guideGeet(): Unit {
+    println("<<<Geet - Git 따라 만들면서 파악하기>>>")
+    println()
+
+    println("현재 사용 가능한 명령어 목록")
+    println("   init     현재 디렉토리에 새로운 Geet 저장소를 초기화합니다.")
+    println()
+
+    println("----------------------------------------")
+    println("깃헙 저장소 링크 - https://github.com/SongJSeop/Geet")
+    println("학습 및 개발 기록 링크 - https://abyssinian-cherry-9fc.notion.site/Geet-Git-2442af8184ee48c6ae8eb5990ff7652d")
 }
