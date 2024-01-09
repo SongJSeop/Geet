4c5874feb04bdf552da8d13a8841afc0c5fb2f5b
송준섭 <wnstjq7979@gmail.com>
2024-01-09 16:27:05 +0900

    feat: GeetInit 구현
    - git init과 똑같이 필요 파일 및 폴더 추가

--- src/main/kotlin/geet/commands/GeetInit.kt
+++ src/main/kotlin/geet/commands/GeetInit.kt
@@ -9,5 +9,24 @@ fun geetInit(): Unit {
     }
 
     File(".geet").mkdir()
+    File(".geet/HEAD").writeText("ref: refs/heads/master")
+    File(".geet/config").writeText("""
+        [core]
+            repositoryformatversion = 0
+            filemode = true
+            bare = false
+            logallrefupdates = true
+            ignorecase = true
+            precomposeunicode = true
+    """.trimIndent())
+    File(".geet/description").writeText("Unnamed geet repository; edit this file 'description' to name the repository.")
+    File(".geet/hooks").mkdir()
+    File(".geet/info").mkdir()
+    File(".geet/objects").mkdir()
+    File(".geet/objects/info").mkdir()
+    File(".geet/objects/pack").mkdir()
+    File(".geet/refs").mkdir()
+    File(".geet/refs/heads").mkdir()
+    File(".geet/refs/tags").mkdir()
     println("Geet 저장소를 초기화했습니다.")
 }
