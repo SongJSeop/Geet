d848eb70c1c5fd66e4f26f66a01738d6f12fca9a
송준섭 <wnstjq7979@gmail.com>
2024-01-09 20:03:30 +0900

    feat: data class GeetHashObjectOptions 추가
    - 개체 타입을 나타내는 type 속성
    - 저장 여부를 나타내는 write 속성
    - 파일 경로를 나타내는 path 속성

--- src/main/kotlin/geet/commands/plumbing/GeetHashObject.kt
+++ src/main/kotlin/geet/commands/plumbing/GeetHashObject.kt
@@ -4,3 +4,9 @@ fun geetHashObject(commandLines: Array<String>) {
     println("hash-object")
     commandLines.forEach { println(it) }
 }
+
+data class GeetHashObjectOptions(
+    var type: String = "blob",
+    var write: Boolean = false,
+    var path: String? = null
+)
