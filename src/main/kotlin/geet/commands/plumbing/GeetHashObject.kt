13a36fb7e613e5d0edd2a0fd76edee1061126bee
송준섭 <wnstjq7979@gmail.com>
2024-01-09 20:09:24 +0900

    feat: getHashObjectOptions 함수 추가
    - '-t' 옵션에 대한 처리 구현

--- src/main/kotlin/geet/commands/plumbing/GeetHashObject.kt
+++ src/main/kotlin/geet/commands/plumbing/GeetHashObject.kt
@@ -1,12 +1,37 @@
 package geet.commands.plumbing
 
-fun geetHashObject(commandLines: Array<String>) {
-    println("hash-object")
-    commandLines.forEach { println(it) }
-}
-
 data class GeetHashObjectOptions(
     var type: String = "blob",
     var write: Boolean = false,
     var path: String? = null
 )
+
+fun geetHashObject(commandLines: Array<String>) {
+    val options = getHashObjectOptions(commandLines)
+    println(options)
+}
+
+fun getHashObjectOptions(commandLines: Array<String>): GeetHashObjectOptions {
+    val options = GeetHashObjectOptions()
+
+    var index: Int = 0
+    while (index < commandLines.size) {
+        when (commandLines[index]) {
+            "-t" -> {
+                if (!(commandLines[index + 1] == "blob" ||
+                    commandLines[index + 1] == "tree" ||
+                    commandLines[index + 1] == "commit" ||
+                    commandLines[index + 1] == "tag")) {
+                    println("'-t' 옵션에 대하여 올바른 개체 타입이 지정되지 않았습니다.: ${commandLines[index + 1]}")
+                }
+
+                options.type = commandLines[index + 1]
+                index += 2
+            }
+            "-w" -> {}
+            else -> {}
+        }
+    }
+
+    return options
+}
