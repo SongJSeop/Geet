cf4b6a681c9c511c4dfd2b7770a90734161fe565
송준섭 <wnstjq7979@gmail.com>
2024-01-10 22:27:18 +0900

    feat: 명령어 실행 전에 geet 저장소가 맞는지 확인하는 과정 추가
    - .geet 폴더가 존재하는지 검사

--- src/main/kotlin/geet/ProcessGeet.kt
+++ src/main/kotlin/geet/ProcessGeet.kt
@@ -2,15 +2,25 @@ package geet
 
 import geet.commands.porcelain.*
 import geet.commands.plumbing.*
+import java.io.File
 
 fun processGeet(commandLines: Array<String>): Unit {
-    if (commandLines.isEmpty()) {
+    if (commandLines.isEmpty() || commandLines[0] == "help") {
         guideGeet()
         return
     }
 
+    if (commandLines[0] == "init") {
+        geetInit()
+        return
+    }
+
+    if (!File(".geet").exists()) {
+        println("Geet 저장소가 존재하지 않습니다.")
+        return
+    }
+
     when (commandLines[0]) {
-        "init" -> geetInit()
         "hash-object" -> geetHashObject(commandLines)
         else -> println("'geet ${commandLines[0]}'은 지원하는 명령어가 아닙니다.")
     }
@@ -22,6 +32,7 @@ fun guideGeet(): Unit {
     println()
 
     println("| 현재 사용 가능한 명령어 목록 |")
+    println("|  help  |  사용 가능한 Geet 명령어 목록을 출력합니다.  |")
     println("|  init  |  현재 디렉토리에 새로운 Geet 저장소를 초기화합니다.  |")
     println("|  hash-object  |  파일을 해시하여 Geet 저장소에 저장합니다.  |")
     println()
