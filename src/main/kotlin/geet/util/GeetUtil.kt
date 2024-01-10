90600b136fa0c57fc8fc734094fc4d8484f37672
송준섭 <wnstjq7979@gmail.com>
2024-01-10 21:58:24 +0900

    feat: 문자열 zlib으로 압축 및 압축 해제하는 함수 추가
    - GeetUtil.kt에 문자열을 zlib으로 압축하는 compressToZlib 함수 추가
    - GeetUtil.kt에 압축을 해제하는 decompressFromZlib 함수 추가

--- src/main/kotlin/geet/util/GeetUtil.kt
+++ src/main/kotlin/geet/util/GeetUtil.kt
@@ -2,7 +2,14 @@ package geet.util
 
 import geet.commands.plumbing.GeetHashObjectOptions
 import geet.commands.plumbing.messageDigest
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.util.Base64
+import java.util.zip.Deflater
+import java.util.zip.DeflaterOutputStream
+import java.util.zip.Inflater
+import java.util.zip.InflaterInputStream
 
 fun isGeetObjectType(type: String): Boolean {
     val typeLowerCase = type.lowercase()
@@ -30,4 +37,32 @@ fun hashObject(options: GeetHashObjectOptions): String {
     return hash.joinToString("") {
         String.format("%02x", it)
     }
+}
+
+fun compressToZlib(contents: String): String {
+    val inputData = contents.toByteArray()
+    val outputStream = ByteArrayOutputStream()
+    val deflater = Deflater()
+    val deflaterOutputStream = DeflaterOutputStream(outputStream, deflater)
+
+    deflaterOutputStream.write(inputData)
+    deflaterOutputStream.close()
+
+    return Base64.getEncoder().encodeToString(outputStream.toByteArray())
+}
+
+fun decompressFromZlib(zlibContents: String): String {
+    val decodedByteArray = Base64.getDecoder().decode(zlibContents)
+    val inputStream = ByteArrayInputStream(decodedByteArray)
+    val inflater = Inflater()
+    val inflaterInputStream = InflaterInputStream(inputStream, inflater)
+    val outputStream = ByteArrayOutputStream()
+
+    val buffer = ByteArray(1024)
+    var length: Int
+    while (inflaterInputStream.read(buffer).also { length = it } > 0) {
+        outputStream.write(buffer, 0, length)
+    }
+
+    return outputStream.toString()
 }
