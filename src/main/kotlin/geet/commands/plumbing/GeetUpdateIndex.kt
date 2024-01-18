1cb84c84713f5211363203c36dd404ae19398bf4
송준섭 <wnstjq7979@gmail.com>
2024-01-18 16:43:12 +0900

    feat: getUpdateIndexOptions 함수 추가
    - data class GeetUpdateIndexOptions 추가
    - 커맨드라인을 읽어 입력 옵션을 읽어오는 getUpdateIndexOptions 함수 추가

--- src/main/kotlin/geet/commands/plumbing/GeetUpdateIndex.kt
+++ src/main/kotlin/geet/commands/plumbing/GeetUpdateIndex.kt
@@ -1,5 +1,40 @@
 package geet.commands.plumbing
 
+import geet.exception.BadRequestException
+
+data class GeetUpdateIndexOptions(
+    var option: String = "",
+    var path: String = "",
+)
+
 fun geetUpdateIndex(commandLines: Array<String>) {
-    println("GeetUpdateIndex")
+    val options = getUpdateIndexOptions(commandLines)
+    println(options)
+}
+
+fun getUpdateIndexOptions(commandLines: Array<String>): GeetUpdateIndexOptions {
+    val options = GeetUpdateIndexOptions()
+
+    var index: Int = 1
+    while (index < commandLines.size) {
+        when (commandLines[index]) {
+            "--add", "--remove", "--refresh" -> {
+                if (options.option != "") {
+                    throw BadRequestException("이미 '${options.option}' 옵션이 지정되었습니다. : ${commandLines[index]}")
+                }
+
+                options.option = commandLines[index]
+            }
+            else -> {
+                if (options.path != "") {
+                    throw BadRequestException("지원하지 않는 옵션이거나 중복된 파일 경로입니다. : ${commandLines[index]}")
+                }
+
+                options.path = commandLines[index]
+            }
+        }
+        index += 1
+    }
+
+    return options
 }
