6035143c848f60ea7e945b51a5d4278dc2113936
송준섭 <wnstjq7979@gmail.com>
2024-01-30 14:05:37 +0900

    feat: GeetStatusResult 데이터 클래스 추가

--- src/main/kotlin/geet/commands/porcelain/geetStatus.kt
+++ src/main/kotlin/geet/commands/porcelain/geetStatus.kt
@@ -3,7 +3,18 @@ package geet.commands.porcelain
 import geet.utils.getNotIgnoreFiles
 import java.io.File
 
+data class GeetStatusResult(
+    val modifiedFiles: MutableList<File> = mutableListOf(),
+    val newFiles: MutableList<File> = mutableListOf(),
+    val removedFiles: MutableList<File> = mutableListOf(),
+    val untrackedFiles: MutableList<File> = mutableListOf(),
+)
+
 fun geetStatus(commandLines: Array<String>): Unit {
     val notIgnoreFiles = getNotIgnoreFiles(startDir = File("."))
     println(notIgnoreFiles)
+}
+
+fun getGeetStatusResult(notIgnoreFiles: List<File>): GeetStatusResult {
+    return GeetStatusResult()
 }
