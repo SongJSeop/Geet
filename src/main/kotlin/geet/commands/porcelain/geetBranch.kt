18c64bc0310a24a8f388b24fa825d5936631cd87
송준섭 <wnstjq7979@gmail.com>
2024-02-18 20:58:15 +0900

    feat: 커맨드 라인으로 옵션값을 읽어오는 getGeetBranchOptions 함수 구현

--- src/main/kotlin/geet/commands/porcelain/geetBranch.kt
+++ src/main/kotlin/geet/commands/porcelain/geetBranch.kt
@@ -1,5 +1,29 @@
 package geet.commands.porcelain
 
+import geet.exceptions.BadRequest
+
+data class GeetBranchOptions(
+    val branchName: String? = null,
+    val delete: Boolean = false
+)
+
 fun geetBranch(commandLines: Array<String>) {
-    println("geetBranch")
+    val geetBranchOptions = getGeetBranchOptions(commandLines)
+    println(geetBranchOptions)
+}
+
+fun getGeetBranchOptions(commandLines: Array<String>): GeetBranchOptions {
+    if (commandLines.size == 1) {
+        return GeetBranchOptions()
+    }
+
+    if (commandLines.size == 2) {
+        return GeetBranchOptions(branchName = commandLines[1])
+    }
+
+    if (commandLines.size == 3 && commandLines[1] == "-d") {
+        return GeetBranchOptions(branchName = commandLines[2], delete = true)
+    }
+
+    throw BadRequest("branch 명령어에 대하여 올바르지 않은 옵션입니다. : ${commandLines.joinToString(" ")}")
 }
