a399e4feeae537627263eca9aa39d0322e2a25bd
송준섭 <wnstjq7979@gmail.com>
2024-02-18 18:36:52 +0900

    feat: geetLog 구현 완료

--- src/main/kotlin/geet/commands/porcelain/geetLog.kt
+++ src/main/kotlin/geet/commands/porcelain/geetLog.kt
@@ -1,5 +1,22 @@
 package geet.commands.porcelain
 
+import geet.exceptions.BadRequest
+import geet.utils.commandutil.porcelainutil.getCommitObjectData
+import geet.utils.commandutil.porcelainutil.getCurrentRefCommitHash
+
 fun geetLog(commandLines: Array<String>): Unit {
-    println("geetLog")
+    if (commandLines.size != 1) {
+        throw BadRequest("log 명령어는 다른 옵션을 받지 않습니다.")
+    }
+
+    var commitHash = getCurrentRefCommitHash()
+    while (true) {
+        val commitObjectData = getCommitObjectData(commitHash)
+        println("commit ${commitHash}")
+        println("Date: ${commitObjectData.datetime}")
+        println()
+        println("\t${commitObjectData.message}")
+        println()
+        commitHash = commitObjectData.parent ?: break
+    }
 }
--- src/main/kotlin/geet/utils/commandutil/porcelainutil/logUtil.kt
+++ src/main/kotlin/geet/utils/commandutil/porcelainutil/logUtil.kt
@@ -1,6 +1,7 @@
 package geet.utils.commandutil.porcelainutil
 
 import geet.utils.GEET_OBJECTS_DIR_PATH
+import geet.utils.decompressFromZlib
 import java.io.File
 import java.time.LocalDateTime
 
@@ -15,7 +16,7 @@ fun getCommitObjectData(commitHash: String): CommitObjectData {
     val dirName = commitHash.substring(0, 2)
     val fileName = commitHash.substring(2)
     val file = File("${GEET_OBJECTS_DIR_PATH}/$dirName/$fileName")
-    val content = file.readText().trim()
+    val content = decompressFromZlib(file.readText()).trim()
 
     val splitContent = content.split("\n")
     val tree = splitContent[0].split(" ")[1]
@@ -23,7 +24,7 @@ fun getCommitObjectData(commitHash: String): CommitObjectData {
     var datetime: LocalDateTime
     var message: String
 
-    if (splitContent.size == 4) {
+    if (splitContent[3] == "") {
         parent = splitContent[1].split(" ")[1]
         datetime = LocalDateTime.parse(splitContent[2].split(" ")[1])
         message = splitContent.subList(4, splitContent.size).joinToString("\n")
