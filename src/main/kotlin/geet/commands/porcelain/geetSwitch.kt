84bbe3159756bffdadfc0ddfc851026c1a9ee64b
송준섭 <wnstjq7979@gmail.com>
2024-02-19 16:41:35 +0900

    feat: switch 명령어에 대한 옵션 입력 처리

--- src/main/kotlin/geet/commands/porcelain/geetSwitch.kt
+++ src/main/kotlin/geet/commands/porcelain/geetSwitch.kt
@@ -1,5 +1,19 @@
 package geet.commands.porcelain
 
+import geet.exceptions.BadRequest
+
+data class GeetSwitchOptions(
+    val branchName: String,
+    val create: Boolean,
+)
+
 fun geetSwitch(commandLines: Array<String>) {
-    println("geetSwitch")
+    var geetSwitchOptions: GeetSwitchOptions
+    if (commandLines.size == 2) {
+        geetSwitchOptions = GeetSwitchOptions(commandLines[1], false)
+    } else if (commandLines.size == 3 && commandLines[1] == "-c") {
+        geetSwitchOptions = GeetSwitchOptions(commandLines[2], true)
+    } else throw BadRequest("switch 명령어에 대한 옵션이 잘못되었습니다.: ${commandLines.joinToString(" ")}")
+
+    switch(geetSwitchOptions)
 }
