09e6b159089b22b76f762e5d5b440b82c59624f9
송준섭 <wnstjq7979@gmail.com>
2024-02-26 18:13:28 +0900

    feat: 입력한 merge 명령어의 옵션을 받아오는 getGeetMergeOptions 함수 구현

--- src/main/kotlin/geet/commands/porcelain/geetMerge.kt
+++ src/main/kotlin/geet/commands/porcelain/geetMerge.kt
@@ -1,5 +1,29 @@
 package geet.commands.porcelain
 
+import geet.exceptions.BadRequest
+
+data class GeetMergeOptions(
+    val branchName: String? = null,
+    val option: String? = null
+)
+
 fun geetMerge(commandLines: Array<String>): Unit {
-    println("geetMerge")
+    val geetMergeOptions = getGeetMergeOptions(commandLines)
+    println(geetMergeOptions)
+}
+
+fun getGeetMergeOptions(commandLines: Array<String>): GeetMergeOptions {
+    if (commandLines.size == 1) {
+        throw BadRequest("병합할 브랜치 이름을 함께 입력하세요.")
+    }
+
+    if (commandLines.size > 3) {
+        throw BadRequest("옵션을 잘못 입력하셨습니다.")
+    }
+
+    when (val command = commandLines[1]) {
+        "--abort" -> return GeetMergeOptions(option = "abort")
+        "--continue" -> return GeetMergeOptions(option = "continue")
+        else -> return GeetMergeOptions(branchName = command)
+    }
 }
