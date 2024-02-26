b7cafef84fa8e2528ea68ac82c2591a9b46fa959
송준섭 <wnstjq7979@gmail.com>
2024-02-26 19:27:09 +0900

    feat: 특정 브랜치의 커밋 해시값을 가져오는 getBranchCommitHash 함수 구현

--- src/main/kotlin/geet/utils/commandutil/porcelainutil/mergeUtil.kt
+++ src/main/kotlin/geet/utils/commandutil/porcelainutil/mergeUtil.kt
@@ -1,14 +1,29 @@
 package geet.utils.commandutil.porcelainutil
 
 import geet.commands.porcelain.GeetMergeOptions
+import geet.exceptions.NotFound
+import geet.utils.GEET_REFS_HEADS_DIR_PATH
+import java.io.File
 
 fun merge(geetMergeOptions: GeetMergeOptions): Unit {
     if (geetMergeOptions.branchName != null) {
-        println("병합할 브랜치: ${geetMergeOptions.branchName}")
+        val currentCommitHash = getCurrentRefCommitHash()
+        val branchCommitHash = getBranchCommitHash(geetMergeOptions.branchName)
+        println("병합 대상 브랜치의 커밋 해시: ${branchCommitHash}")
+        println("현재 브랜치의 커밋 해시: ${currentCommitHash}")
     }
 
     when (geetMergeOptions.option) {
         "abort" -> println("병합을 중단합니다.")
         "continue" -> println("병합을 계속합니다.")
     }
+}
+
+fun getBranchCommitHash(branchName: String): String {
+    val refFile = File("${GEET_REFS_HEADS_DIR_PATH}/${branchName}")
+    if (!refFile.exists()) {
+        throw NotFound("존재하지 않는 브랜치입니다.: ${branchName}")
+    }
+
+    return refFile.readText()
 }
