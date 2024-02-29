6b7448c93f9b0679cdd503a2da868e31571256c0
송준섭 <wnstjq7979@gmail.com>
2024-02-29 16:48:39 +0900

    feat: 지원하지 않는 명령어인 경우 에러 처리

--- src/main/kotlin/geet/processGeet.kt
+++ src/main/kotlin/geet/processGeet.kt
@@ -1,9 +1,15 @@
 package geet
 
 import geet.command.geetHelp
+import geet.exception.BadRequest
 
 fun processGeet(commandLines: Array<String>): Unit {
     if (commandLines.isEmpty() || commandLines[0] == "help") {
         geetHelp()
+        return
+    }
+
+    when (commandLines[0]) {
+        else -> throw BadRequest("지원하지 않는 명령어입니다.: ${commandLines[0]}")
     }
 }
