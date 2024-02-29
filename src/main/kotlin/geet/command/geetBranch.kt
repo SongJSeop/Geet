d140fd3c71e3cb1782b950dd5da065d8c08c2ed3
송준섭 <wnstjq7979@gmail.com>
2024-02-29 22:23:14 +0900

    feat: branch 명령어를 처리하기 위한 geetBranch 추가 및 설명 추가

---
+++ src/main/kotlin/geet/command/geetBranch.kt
@@ -0,0 +1,5 @@
+package geet.command
+
+fun geetBranch(commandLines: Array<String>): Unit {
+    println("geetBranch")
+}
--- src/main/kotlin/geet/processGeet.kt
+++ src/main/kotlin/geet/processGeet.kt
@@ -13,6 +13,7 @@ fun processGeet(commandLines: Array<String>): Unit {
 
     when (commandLines[0]) {
         "init" -> geetInit(commandLines)
+        "branch" -> geetBranch(commandLines)
         else -> throw BadRequest("지원하지 않는 명령어입니다.: ${red}${commandLines[0]}${resetColor}")
     }
 }
