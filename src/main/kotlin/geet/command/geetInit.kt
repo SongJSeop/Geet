f5c642665ea7de45c4c31685b26ec7c916cee816
송준섭 <wnstjq7979@gmail.com>
2024-02-29 18:42:32 +0900

    feat: init 명령어 구현

--- src/main/kotlin/geet/command/geetInit.kt
+++ src/main/kotlin/geet/command/geetInit.kt
@@ -1,5 +1,43 @@
 package geet.command
 
+import geet.exception.BadRequest
+import geet.util.const.red
+import geet.util.const.resetColor
+import java.io.File
+
 fun geetInit(commandLines: Array<String>): Unit {
-    println("geetInit")
+    if (commandLines.size != 1) {
+        throw BadRequest(
+            "init 명령어는 다른 옵션을 받지 않습니다.: " +
+                    "${red}${commandLines.toList().subList(1, commandLines.size).joinToString(" ")}${resetColor}"
+        )
+    }
+
+    val geetFile = File(".geet")
+    if (geetFile.exists()) {
+        throw BadRequest("이미 초기화된 Git 저장소 입니다.")
+    }
+
+    println("현재 디렉토리를 Git 저장소로 초기화합니다.")
+    geetFile.mkdir()
+    File(geetFile, "objects").mkdir()
+    File(geetFile, "objects/info").mkdir()
+    File(geetFile, "objects/pack").mkdir()
+    File(geetFile, "refs").mkdir()
+    File(geetFile, "refs/heads").mkdir()
+    File(geetFile, "refs/tags").mkdir()
+    File(geetFile, "HEAD").writeText("ref: refs/heads/master")
+    File(geetFile, "config").writeText(
+        """
+        [core]
+            repositoryformatversion = 0
+            filemode = true
+            bare = false
+            logallrefupdates = true
+        """.trimIndent()
+    )
+    File(geetFile, "description").writeText("Unnamed repository; edit this file 'description' to name the repository.")
+    File(geetFile, "hooks").mkdir()
+    File(geetFile, "info").mkdir()
+    File(geetFile, "info/exclude").writeText("# *.[oa]\n# *~")
 }
--- src/main/kotlin/geet/processGeet.kt
+++ src/main/kotlin/geet/processGeet.kt
@@ -2,6 +2,8 @@ package geet
 
 import geet.command.*
 import geet.exception.BadRequest
+import geet.util.const.red
+import geet.util.const.resetColor
 
 fun processGeet(commandLines: Array<String>): Unit {
     if (commandLines.isEmpty() || commandLines[0] == "help") {
@@ -11,6 +13,6 @@ fun processGeet(commandLines: Array<String>): Unit {
 
     when (commandLines[0]) {
         "init" -> geetInit(commandLines)
-        else -> throw BadRequest("지원하지 않는 명령어입니다.: ${commandLines[0]}")
+        else -> throw BadRequest("지원하지 않는 명령어입니다.: ${red}${commandLines[0]}${resetColor}")
     }
 }
