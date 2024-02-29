17342b65a1b9663c1e4f502be0c55c0a19dac1fc
송준섭 <wnstjq7979@gmail.com>
2024-02-29 23:05:30 +0900

    feat: BranchManager에 브랜치를 생성하는 createBranch 함수 추가

--- src/main/kotlin/geet/manager/BranchManager.kt
+++ src/main/kotlin/geet/manager/BranchManager.kt
@@ -1,5 +1,6 @@
 package geet.manager
 
+import geet.util.const.headManager
 import java.io.File
 
 class BranchManager {
@@ -14,5 +15,19 @@ class BranchManager {
     fun setBranchCommitHash(branchName: String, commitHash: String) {
         val branchFile = File(refsDir, "heads/$branchName")
         branchFile.writeText(commitHash)
+    }
+
+    fun createBranch(branchName: String) {
+        if (branchName.contains("/")) {
+            val branchNameList = branchName.split("/")
+            val branchDir = File(refsDir, "heads/${branchNameList[0]}")
+            branchDir.mkdirs()
+            createBranch(branchNameList.subList(1, branchNameList.size).joinToString("/"))
+        } else {
+            val branchFile = File(refsDir, "heads/$branchName")
+            val headBranchName = headManager.getHeadBranchName()
+            val headCommitHash = getBranchCommitHash(headBranchName)
+            branchFile.writeText(headCommitHash)
+        }
     }
 }
