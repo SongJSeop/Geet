dd14d584b1347dce7b1c756c61fff6309381ab24
송준섭 <wnstjq7979@gmail.com>
2024-03-01 00:20:27 +0900

    feat: BranchManager 함수들 에러 처리

--- src/main/kotlin/geet/manager/BranchManager.kt
+++ src/main/kotlin/geet/manager/BranchManager.kt
@@ -1,6 +1,10 @@
 package geet.manager
 
+import geet.exception.BadRequest
+import geet.exception.NotFound
 import geet.util.const.headManager
+import geet.util.const.red
+import geet.util.const.resetColor
 import java.io.File
 
 class BranchManager {
@@ -9,15 +13,27 @@ class BranchManager {
 
     fun getBranchCommitHash(branchName: String): String {
         val branchFile = File(refsDir, "heads/$branchName")
+        if (!branchFile.exists()) {
+            throw NotFound("브랜치가 존재하지 않습니다.: ${red}${branchName}${resetColor}")
+        }
+
         return branchFile.readText()
     }
 
     fun setBranchCommitHash(branchName: String, commitHash: String) {
         val branchFile = File(refsDir, "heads/$branchName")
+        if (!branchFile.exists()) {
+            throw NotFound("브랜치가 존재하지 않습니다.: ${red}${branchName}${resetColor}")
+        }
+
         branchFile.writeText(commitHash)
     }
 
     fun createBranch(branchName: String) {
+        if (File(refsDir, "heads/$branchName").exists()) {
+            throw BadRequest("브랜치가 이미 존재합니다.: ${red}${branchName}${resetColor}")
+        }
+
         if (branchName.contains("/")) {
             val branchNameList = branchName.split("/")
             val branchDir = File(refsDir, "heads/${branchNameList[0]}")
@@ -32,6 +48,10 @@ class BranchManager {
     }
 
     fun switchBranch(branchName: String) {
+        if (!File(refsDir, "heads/$branchName").exists()) {
+            throw NotFound("브랜치가 존재하지 않습니다.: ${red}${branchName}${resetColor}")
+        }
+
         headManager.setHead(branchName)
     }
 }
