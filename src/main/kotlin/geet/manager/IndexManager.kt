1922f30dc9fc551fb48848b447d2ccff0fc2dc0f
송준섭 <wnstjq7979@gmail.com>
2024-03-05 11:34:56 +0900

    feat: index를 관리하는 IndexManager 추가 및 data class StageObject, IndexData 추가

---
+++ src/main/kotlin/geet/manager/IndexManager.kt
@@ -0,0 +1,42 @@
+package geet.manager
+
+import geet.geetobject.GeetBlob
+import geet.geetobject.GeetObjectWithFile
+import geet.util.fromZlibToString
+import geet.util.toZlib
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.io.File
+import java.time.LocalDateTime
+
+@Serializable
+data class StageObject(
+    val hash: String,
+    val slot: Int = 0,
+    val filePath: String,
+    val status: String,
+    val lastUpdateTime: String
+)
+
+@Serializable
+data class IndexData(
+    val stageObjects: MutableList<StageObject>,
+    val lastCommitObjects: List<GeetObjectWithFile>
+)
+
+class IndexManager {
+
+    val indexFile = File(".geet/index")
+    val indexData: IndexData
+        get() {
+            if (!indexFile.exists()) {
+                return IndexData(stageObjects = mutableListOf(), lastCommitObjects = emptyList())
+            }
+
+            return Json.decodeFromString(IndexData.serializer(), indexFile.readText().fromZlibToString())
+        }
+
+    fun writeIndex() {
+        indexFile.writeText(Json.encodeToString(IndexData.serializer(), indexData).toZlib())
+    }
+}
