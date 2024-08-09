a80ba6fdf0b941a25ce370f5adfac06c83f42caf
송준섭 <wnstjq7979@gmail.com>
2024-08-09 09:43:25 +0900

    refactor: createHash 함수 분리

--- src/main/kotlin/geet/geetobject/GeetObject.kt
+++ src/main/kotlin/geet/geetobject/GeetObject.kt
@@ -1,20 +1,12 @@
 package geet.geetobject
 
 import geet.enums.GeetObjectType
-import geet.util.const.messageDigest
+import geet.util.createHash
 
 interface GeetObject {
 
     val type: GeetObjectType
     val content: String
     val hash: String
-        get() {
-            val header = "${type.value} ${content.length}\u0000"
-            val store = header + content
-
-            val hash = messageDigest.digest(store.toByteArray())
-            return hash.joinToString("") {
-                String.format("%02x", it)
-            }
-        }
+        get() = createHash(type, content)
 }
--- src/main/kotlin/geet/util/objectUtil.kt
+++ src/main/kotlin/geet/util/objectUtil.kt
@@ -1,8 +1,20 @@
 package geet.util
 
+import geet.enums.GeetObjectType
+import geet.util.const.messageDigest
 import java.io.File
 
 val objectFile = File(getGeetRepoDir(), "objects")
+
+fun createHash(type: GeetObjectType, content: String): String {
+    val header = "${type.value} ${content.length}\u0000"
+    val store = header + content
+
+    val hash = messageDigest.digest(store.toByteArray())
+    return hash.joinToString("") {
+        String.format("%02x", it)
+    }
+}
 
 fun saveObject(hash: String, content: String) {
     val objectDir = File(objectFile, hash.substring(0, 2))
