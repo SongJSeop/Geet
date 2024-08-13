cc11b8a3b0624e21134f5e1e4472805a950d6cee
송준섭 <wnstjq7979@gmail.com>
2024-08-13 21:46:08 +0900

    feat: getAddOptions 추가

--- src/main/kotlin/geet/command/porcelain/geetAdd.kt
+++ src/main/kotlin/geet/command/porcelain/geetAdd.kt
@@ -1,5 +1,28 @@
 package geet.command.porcelain
 
+import geet.exception.BadRequest
+import geet.util.const.resetColor
+import geet.util.const.weekGreen
+import java.io.File
+
+data class AddOptions(
+    val file: File
+)
+
 fun geetAdd(commandLines: Array<String>): Unit {
-    println("geetAdd")
+    val options = getAddOptions(commandLines)
+    println(options)
+}
+
+fun getAddOptions(commandLines: Array<String>): AddOptions {
+    if (commandLines.size != 2) {
+        throw BadRequest("add 명령어는 파일명만을 인자로 받습니다. 사용법: ${weekGreen}geet add <file>${resetColor}")
+    }
+
+    val file = File(commandLines[1])
+    if (!file.exists()) {
+        throw BadRequest("해당 이름으로 된 파일이 존재하지 않습니다.: ${commandLines[1]}")
+    }
+
+    return AddOptions(file)
 }
